4fad69bdc022320c347cbeb82f480fd18b85cc8a
juliooest <juliocnarciso@hotmail.com>
2025-05-17 14:58:02 -0400

    responsividade

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import NavBar from "../components/NavBar";
 import Hero from "@/components/Hero";
 import SecurityDashboard from "@/components/SecurityDashboard";
@@ -5,6 +6,10 @@ import AboutSection from '@/components/AboutSection';
 import ContactSection from "@/components/ContactSection";
 
 export default function Home() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <>
       <NavBar />
